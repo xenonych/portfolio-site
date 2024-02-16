ea65af03f846dfcb4ba92727e3cb27957d27865b
xenonych <144382517+xenonych@users.noreply.github.com>
2024-02-16 08:47:39 +0300

    site v1

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".images img").click(function(){
+  $("#full-image").attr("src", $(this).attr("src"));
+  $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function(){
+  $('#image-viewer').hide();
+});
